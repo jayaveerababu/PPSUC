72c0f715b848db60aa8574b2cf6024853d326d6c
jayaveerababu <jayaveerababun@gmail.com>
2025-10-23 11:45:54 +0530

    Add files via upload

---
+++ switch case.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+  int main()
+  {
+  	int a,b,c;
+  	char op;
+  	printf("enter operator(+,-,*,/,%):");
+  	scanf("%c",&op);
+  	printf("enter 2 values");
+  	scanf("%d%d",&a,&b);
+  	switch(op)
+  	{
+  		case '+':c=a+b;
+  		printf("addition is:%d",c);
+  		break;
+  		case '-':c=a-b;
+  		printf("subtraction is:%d",c);
+  		break;
+  		case '*':c=a*b;
+  		printf("multipcation is:%d",c);
+  		break;
+  		case '/':c=a/b;
+  		printf("division is:%d",c);
+  		break;
+  	    case '%':c=a%b;
+  		printf("remainder is:%d",c);
+  		break;
+		default:printf("invaild opereator");
+		break;
+	  }
+	  return 0;
+  }
